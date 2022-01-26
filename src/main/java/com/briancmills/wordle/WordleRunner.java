49ffc7abf349e68191a170e00049144c3fde8540
brianmills <brian@marginedge.com>
2022-01-26 12:17:55 -0500

    Small improvements to WorldRunner code.

--- src/main/java/com/briancmills/wordle/WordleRunner.java
+++ src/main/java/com/briancmills/wordle/WordleRunner.java
@@ -26,55 +26,69 @@ public class WordleRunner {
         
         String inputFileName = args[0];
         File file = FileUtils.getFile(inputFileName);
+
+        List<String> wordList = getWordList(inputFileName);
+
+        int randomNum = ThreadLocalRandom.current().nextInt(0, wordList.size() + 1);
+        String answer = wordList.get(randomNum);
         
-        try {
-            List<String> wordList = FileUtils.readLines(file, "UTF-8");
-            //lines.forEach(l->System.out.println(l));
-    
-            int randomNum = ThreadLocalRandom.current().nextInt(0, wordList.size() + 1);
-            String answer = wordList.get(randomNum);
-            Solver solver = new NaiveRandomGuessSolver();
-            solver.initialize(wordList);
-            List<PastWordGuessResult> pastGuessResults = new ArrayList<>();
-            // the solver gets 6 guesses
-            for (int i = 0; i <= 5; i++) {
-                String guess = solver.guess(pastGuessResults).toLowerCase(Locale.ROOT);
-                PastWordGuessResult wordGuessResult = new PastWordGuessResult();
-                // check each letter against the answer
-                for (int j = 0; j <= 4; j++) {
-                    char guessLetter = guess.charAt(j);
-                    char correctLetter = answer.charAt(j);
-                    LetterGuessResult letterGuessResult = LetterGuessResult.LETTER_NOT_PRESENT;
-                    if (guessLetter == correctLetter) {
-                        letterGuessResult = LetterGuessResult.CORRECT_POSITION;
-                    } else if (answer.contains(""+guessLetter)) {
-                        letterGuessResult = LetterGuessResult.INCORRECT_POSITION;
-                    }
-                    wordGuessResult.addLetterResult(new PastLetterGuessResult(j, guessLetter, letterGuessResult));
+        Solver solver = new NaiveRandomGuessSolver();
+        solver.initialize(wordList);
+        List<PastWordGuessResult> pastGuessResults = new ArrayList<>();
+        
+        // the solver gets 6 guesses
+        for (int i = 0; i <= 5; i++) {
+            String guess = solver.guess(pastGuessResults).toLowerCase(Locale.ROOT);
+            PastWordGuessResult wordGuessResult = new PastWordGuessResult();
+            // check each letter against the answer
+            for (int j = 0; j <= 4; j++) {
+                char guessLetter = guess.charAt(j);
+                char correctLetter = answer.charAt(j);
+                LetterGuessResult letterGuessResult = LetterGuessResult.LETTER_NOT_PRESENT;
+                if (guessLetter == correctLetter) {
+                    letterGuessResult = LetterGuessResult.CORRECT_POSITION;
+                } else if (answer.contains(""+guessLetter)) {
+                    letterGuessResult = LetterGuessResult.INCORRECT_POSITION;
                 }
-                
-                pastGuessResults.add(wordGuessResult);
-                
-                if (guess.equalsIgnoreCase(answer)) {
-                    System.out.println("The guess was correct: " + guess);
-                    break;
-                } else {
-                    System.out.println(guess);
-                    wordGuessResult.getPastLetterResults().forEach(r-> {
-                        switch (r.getResult()) {
-                            case CORRECT_POSITION -> System.out.print("+");
-                            case INCORRECT_POSITION -> System.out.print("*");
-                            case LETTER_NOT_PRESENT -> System.out.print("-");
-                        }
-                    });
-                    System.out.println();
-                }
+                wordGuessResult.addLetterResult(new PastLetterGuessResult(j, guessLetter, letterGuessResult));
             }
-    
-            System.out.println("\nThe answer was: "+ answer);
-        } catch (Exception e) {
-            System.err.println("runtime error");
-            e.printStackTrace(System.err);
+            
+            pastGuessResults.add(wordGuessResult);
+            
+            if (guess.equalsIgnoreCase(answer)) {
+                System.out.println("The guess was correct: " + guess);
+                break;
+            } else {
+                System.out.println(guess);
+                wordGuessResult.getPastLetterResults().forEach(r-> {
+                    switch (r.getResult()) {
+                        case CORRECT_POSITION -> System.out.print("+");
+                        case INCORRECT_POSITION -> System.out.print("*");
+                        case LETTER_NOT_PRESENT -> System.out.print("-");
+                    }
+                });
+                System.out.println();
+            }
         }
+
+        System.out.println("\nThe answer was: "+ answer);
+    }
+    
+    /**
+     * Factor out the logic of reading the lines from the input file and error handling. 
+     * 
+     * @param inputFileName The txt file that contains a list of 5-letter words for the puzzle.
+     * @return The list of words for the puzzle. 
+     */
+    private static List<String> getWordList(String inputFileName) {
+        File file = FileUtils.getFile(inputFileName);
+    
+        try {
+            return FileUtils.readLines(file, "UTF-8");
+        } catch (Exception e) {
+            System.err.println("Error opening word file: " + inputFileName);
+            e.printStackTrace(System.err);
+        }  
+        return Collections.emptyList();
     }
 }
