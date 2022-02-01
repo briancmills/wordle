7e92247e9813f1ec081a6360ebeb96439c153723
brianmills <brian@marginedge.com>
2022-02-01 10:04:55 -0500

    Run the game 10k times and measure the success rate.

--- src/main/java/com/briancmills/wordle/WordleRunner.java
+++ src/main/java/com/briancmills/wordle/WordleRunner.java
@@ -14,6 +14,8 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class WordleRunner {
     
+    private static final int NUMBER_OF_GAMES = 10000;
+    
     public static void main(String[] args) {
         
         if (args.length < 1) {
@@ -23,55 +25,31 @@ public class WordleRunner {
         }
     
         System.out.println("Starting WordleRunner: " + args[0]);
+        System.out.println("Will run " + NUMBER_OF_GAMES + " games.");
         
         String inputFileName = args[0];
         File file = FileUtils.getFile(inputFileName);
 
         List<String> wordList = getWordList(inputFileName);
-
-        int randomNum = ThreadLocalRandom.current().nextInt(0, wordList.size() + 1);
-        String answer = wordList.get(randomNum);
         
         Solver solver = new NaiveRandomGuessSolver();
-        solver.initialize(wordList);
-        List<PastWordGuessResult> pastGuessResults = new ArrayList<>();
+        // give the solver an unmodifiable copy of the list
+        solver.initialize(Collections.unmodifiableList(wordList));
         
-        // the solver gets 6 guesses
-        for (int i = 0; i <= 5; i++) {
-            String guess = solver.guess(pastGuessResults).toLowerCase(Locale.ROOT);
-            PastWordGuessResult wordGuessResult = new PastWordGuessResult();
-            // check each letter against the answer
-            for (int j = 0; j <= 4; j++) {
-                char guessLetter = guess.charAt(j);
-                char correctLetter = answer.charAt(j);
-                LetterGuessResult letterGuessResult = LetterGuessResult.LETTER_NOT_PRESENT;
-                if (guessLetter == correctLetter) {
-                    letterGuessResult = LetterGuessResult.CORRECT_POSITION;
-                } else if (answer.contains(""+guessLetter)) {
-                    letterGuessResult = LetterGuessResult.INCORRECT_POSITION;
-                }
-                wordGuessResult.addLetterResult(new PastLetterGuessResult(j, guessLetter, letterGuessResult));
+        // run the game 1000 times and calculate how often the solver is correct
+        int victories = 0;
+        for (int i = 0; i < NUMBER_OF_GAMES; i++) {
+            int randomNum = ThreadLocalRandom.current().nextInt(0, wordList.size());
+            String answer = wordList.get(randomNum);
+            if (runGame(answer, solver, false)) {
+                victories++;
             }
-            
-            pastGuessResults.add(wordGuessResult);
-            
-            if (guess.equalsIgnoreCase(answer)) {
-                System.out.println("The guess was correct: " + guess);
-                break;
-            } else {
-                System.out.println(guess);
-                wordGuessResult.getPastLetterResults().forEach(r-> {
-                    switch (r.getResult()) {
-                        case CORRECT_POSITION -> System.out.print("+");
-                        case INCORRECT_POSITION -> System.out.print("*");
-                        case LETTER_NOT_PRESENT -> System.out.print("-");
-                    }
-                });
-                System.out.println();
-            }
+            // remove the answer from the word list so we don't run the same answer twice
+            wordList.remove(answer);
         }
-
-        System.out.println("\nThe answer was: "+ answer);
+        System.out.println("solver produced " + victories + " victories");
+        
+        System.out.println(String.format("%.2f",((victories / Double.valueOf(NUMBER_OF_GAMES)) * 100)) + "% of games were victories");
     }
     
     /**
@@ -90,5 +68,62 @@ public class WordleRunner {
             e.printStackTrace(System.err);
         }  
         return Collections.emptyList();
+    }
+    
+    /**
+     * Runs a single instance of a wordle game using the given solver. 
+     *
+     * @param answer The correct answer.
+     * @param solver The solver which is going to attempt to guess the word.  Must already be initialized. 
+     * @return A true value indicates the solver guessed the word correctly. 
+     */
+    private static boolean runGame(String answer, Solver solver, boolean verbose) {
+        boolean result = false;
+        
+        List<PastWordGuessResult> pastGuessResults = new ArrayList<>();
+    
+        // the solver gets 6 guesses
+        for (int i = 0; i <= 5; i++) {
+            String guess = solver.guess(pastGuessResults).toLowerCase(Locale.ROOT);
+            PastWordGuessResult wordGuessResult = new PastWordGuessResult();
+            // check each letter against the answer
+            for (int j = 0; j <= 4; j++) {
+                char guessLetter = guess.charAt(j);
+                char correctLetter = answer.charAt(j);
+                LetterGuessResult letterGuessResult = LetterGuessResult.LETTER_NOT_PRESENT;
+                if (guessLetter == correctLetter) {
+                    letterGuessResult = LetterGuessResult.CORRECT_POSITION;
+                } else if (answer.contains(""+guessLetter)) {
+                    letterGuessResult = LetterGuessResult.INCORRECT_POSITION;
+                }
+                wordGuessResult.addLetterResult(new PastLetterGuessResult(j, guessLetter, letterGuessResult));
+            }
+        
+            pastGuessResults.add(wordGuessResult);
+        
+            if (guess.equalsIgnoreCase(answer)) {
+                if (verbose)  {
+                    System.out.println("The guess was correct: " + guess);
+                }
+                result = true;
+                break;
+            } else {
+                if (verbose) {
+                    System.out.println(guess);
+                    wordGuessResult.getPastLetterResults().forEach(r-> {
+                        switch (r.getResult()) {
+                            case CORRECT_POSITION -> System.out.print("+");
+                            case INCORRECT_POSITION -> System.out.print("*");
+                            case LETTER_NOT_PRESENT -> System.out.print("-");
+                        }
+                    });
+                    System.out.println();
+                }
+            }
+        }
+        if (verbose) {
+            System.out.println("\nThe answer was: " + answer);
+        }
+        return result;
     }
 }
