b4047c68f3e24c631d37fb4099379168c42386c8
joshuadavy <91229371+joshuadavy@users.noreply.github.com>
2022-02-07 21:07:50 -0500

    I broke up the filtering logic into separate methods, and I have a created some files for testing the filters, however I am unsure on how to actually test each method. Do I need to make a separate class for each method so they can be tested individually, or can I do that from the larger HopefullySmartSolver class?

--- src/main/java/com/briancmills/wordle/solver/HopefullySmartSolver.java
+++ src/main/java/com/briancmills/wordle/solver/HopefullySmartSolver.java
@@ -6,6 +6,7 @@ import com.briancmills.wordle.PastWordGuessResult;
 
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class HopefullySmartSolver implements Solver {
@@ -20,99 +21,98 @@ public class HopefullySmartSolver implements Solver {
     @Override
     public String guess(List<PastWordGuessResult> pastGuessResults) {
 
-      List<String> foundLetterCorrectPosition = new ArrayList<>();
-      List<String> foundLetterIncorrectPosition = new ArrayList<>();
+        List<String> foundLetterCorrectPosition = new ArrayList<>();
+        List<String> foundLetterIncorrectPosition = new ArrayList<>();
 
-      Set<String> wrongLetter = new HashSet<>();
+        Set<String> wrongLetter = new HashSet<>();
 
-      Map<Integer, Character> incorrectPositionMap = new HashMap<>();
-      Map<Integer, Character> correctPositionMap = new HashMap<>();
+        Map<Integer, Character> incorrectPositionMap = new HashMap<>();
+        Map<Integer, Character> correctPositionMap = new HashMap<>();
 
-      for (PastWordGuessResult pastResult: pastGuessResults) {
-          for (PastLetterGuessResult pastLetterResult: pastResult.getPastLetterResults()) {
-              if (pastLetterResult.getResult() == LetterGuessResult.CORRECT_POSITION) {
-                  foundLetterCorrectPosition.add("" + pastLetterResult.getLetter());
-                  correctPositionMap.put(pastLetterResult.getPosition(), pastLetterResult.getLetter());
-              }
-              if (pastLetterResult.getResult() == LetterGuessResult.INCORRECT_POSITION) {
-                  foundLetterIncorrectPosition.add("" + pastLetterResult.getLetter());
-                  incorrectPositionMap.put(pastLetterResult.getPosition(), pastLetterResult.getLetter());
-              }
-              if (pastLetterResult.getResult() == LetterGuessResult.LETTER_NOT_PRESENT) {
-                  wrongLetter.add("" + pastLetterResult.getLetter());
-              }
-          }
-      }
-      List<String> filteredWordList = wordList.stream()
-              .filter(
-                      word -> {
-                          // I am going to use a map
-                          int matchCount = 0;
-                          for (String letter: foundLetterCorrectPosition) {
-                              if (word.contains(letter)) {
-                                  matchCount++;
-                              }
-                          }
-                          return matchCount == foundLetterCorrectPosition.size();
-                      }
-              ).filter(
-                      word -> {
-                          int matchCount = 0;
-                          for (String letter: foundLetterIncorrectPosition) {
-                              if (word.contains(letter)){
-                                  matchCount++;
-                              }
-                          }
-                          return matchCount == foundLetterIncorrectPosition.size();
-                      }
 
-              ).filter(
-                      word -> {
-                          boolean match = true;
-                          for (String letter : wrongLetter) {
-                              if (word.contains(letter)) {
-                                   match = false;
-                                   break;
-                              }
-                          }
-                          return match;
-                      }
-
-              ).filter(
-                      word -> {
-
-                          boolean match = true;
-
-                          for (Integer position : correctPositionMap.keySet()) {
-                             if (word.charAt(position) != correctPositionMap.get(position)) {
-                                 match = false;
-                             }
-                          }
-                          return match;
-                      }
-
-              ).filter(
-                word -> {
-
-                    boolean match = true;
-
-                    for (Integer position : incorrectPositionMap.keySet()) {
-                        if (word.charAt(position) == incorrectPositionMap.get(position)) {
-                            match = false;
-                        }
-                    }
-                    return match;
+        for (PastWordGuessResult pastResult : pastGuessResults) {
+            for (PastLetterGuessResult pastLetterResult : pastResult.getPastLetterResults()) {
+                if (pastLetterResult.getResult() == LetterGuessResult.CORRECT_POSITION) {
+                    foundLetterCorrectPosition.add("" + pastLetterResult.getLetter());
+                    correctPositionMap.put(pastLetterResult.getPosition(), pastLetterResult.getLetter());
                 }
-
-        ).collect(Collectors.toList());
-
-            if (pastGuessResults.isEmpty()) {
-                String firstWord = "tares";
-                return firstWord;
+                if (pastLetterResult.getResult() == LetterGuessResult.INCORRECT_POSITION) {
+                    foundLetterIncorrectPosition.add("" + pastLetterResult.getLetter());
+                    incorrectPositionMap.put(pastLetterResult.getPosition(), pastLetterResult.getLetter());
+                }
+                if (pastLetterResult.getResult() == LetterGuessResult.LETTER_NOT_PRESENT) {
+                    wrongLetter.add("" + pastLetterResult.getLetter());
+                }
             }
-            else {
-                int randomNum = ThreadLocalRandom.current().nextInt(0, filteredWordList.size());
-                return filteredWordList.get(randomNum);
+        }
+
+         Predicate<? super String> wrongLetterFilter = word -> {
+            boolean match = true;
+            for (String letter : wrongLetter) {
+                if (word.contains(letter)) {
+                    match = false;
+                    break;
+                }
             }
+            return match;
+        };
+
+        Predicate<? super String> correctLetterFilter = word -> {
+            int matchCount = 0;
+            for (String letter : foundLetterCorrectPosition) {
+                if (word.contains(letter)) {
+                    matchCount++;
+                }
+            }
+            return matchCount == foundLetterCorrectPosition.size();
+        };
+
+        Predicate<? super String> incorrectLetterFilter = word -> {
+            int matchCount = 0;
+            for (String letter : foundLetterIncorrectPosition) {
+                if (word.contains(letter)) {
+                    matchCount++;
+                }
+            }
+            return matchCount == foundLetterIncorrectPosition.size();
+        };
+
+        Predicate<? super String> correctPositionFilter = word -> {
+
+            boolean match = true;
+
+            for (Integer position : correctPositionMap.keySet()) {
+                if (word.charAt(position) != correctPositionMap.get(position)) {
+                    match = false;
+                }
+            }
+            return match;
+        };
+
+        Predicate<? super String> incorrectPositionFilter = word -> {
+
+            boolean match = true;
+
+            for (Integer position : incorrectPositionMap.keySet()) {
+                if (word.charAt(position) == incorrectPositionMap.get(position)) {
+                    match = false;
+                }
+            }
+            return match;
+        };
+
+        List<String> filteredWordList = wordList.stream()
+                .filter(correctLetterFilter).filter(incorrectLetterFilter).
+                filter(wrongLetterFilter).filter(correctPositionFilter
+                ).filter(incorrectPositionFilter).collect(Collectors.toList());
+
+        if (pastGuessResults.isEmpty()) {
+            String firstWord = "tares";
+            return firstWord;
+        } else {
+            int randomNum = ThreadLocalRandom.current().nextInt(0, filteredWordList.size());
+            return filteredWordList.get(randomNum);
+        }
     }
 }
+
